02dbc91c83016abb4a5f1edc0af087677c8317aa
wwu94 <wuwenjun1995@gmail.com>
2017-11-17 12:40:47 -0800

    Added feature dimension reduction after data splitting

--- main.py
+++ main.py
@@ -112,7 +112,10 @@ resized_labels = np.resize(Y, (Y.shape[0]*Y.shape[1],1))
 
 
 # Split into training and test set
-[X_train, Y_train , X_validate, Y_validate ,X_test , Y_test] = dataset_split(resized_features,resized_labels,0.9,0.8)              
+[X_train, Y_train , X_validate, Y_validate ,X_test , Y_test] = dataset_split(resized_features,
+resized_labels,0.9,0.8)        
+# reduce feature dimension
+[pca_train, Y_train, pca_validate, Y_validate, pca_test, Y_test] = reduceFeatures(X_train, Y_train , X_validate, Y_validate ,X_test , Y_test)      
 
 # Fit a simple decision tree first
 base_tree = DecisionTreeClassifier(max_depth = 1, random_state = 1)
